6047877ff505cd835ad1191465ffb2a4d4f33dd7
Maria Serykh <marie.serykh@gmail.com>
2022-05-05 22:04:29 +0200

    refactor: add mock logout

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useScroll } from '../../hooks/useScroll';
 import Button from '../../components/Button/Button';
 import LanguageToggle from '../../components/LanguageToggle/LanguageToggle';
 import Logo from '../../components/Logo/Logo';
 import UserAvatar from '../../components/UserAvatar/UserAvatar';
+import { useState } from 'react';
+
+export const userState = {
+  isAuthorised: true,
+}; //state.user.isAutorised
 
 const Header = (): JSX.Element => {
   const isScrolling = useScroll();
 
-  const isAuthorised = true; //state.user.isAutorised
+  const [isAuthorised, setIsAuthorised] = useState<boolean>(userState.isAuthorised);
+
   const AuthorisedButtons = (): JSX.Element => {
     if (isAuthorised) {
       return (
@@ -28,7 +33,8 @@ const Header = (): JSX.Element => {
           <Button
             onClick={() => {
               // dispatch({type: LOGOUT});
-              console.log('clicked log out');
+              userState.isAuthorised = false;
+              setIsAuthorised(userState.isAuthorised);
             }}
             type="button"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
