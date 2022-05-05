7fc4c1d3311734a4e337ea5df01a241f64b70444
Maria Serykh <marie.serykh@gmail.com>
2022-05-05 13:45:55 +0200

    fix: fix header tests

--- src/layout/Header/Header.test.tsx
+++ src/layout/Header/Header.test.tsx
@@ -3,15 +3,16 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import Header from './Header';
+import { BrowserRouter } from 'react-router-dom';
 
 test('Header rendering', () => {
-  const { getByText } = render(<Header />);
+  const { getByText } = render(<Header />, { wrapper: BrowserRouter });
   expect(getByText(/alldone/i)).toBeInTheDocument();
   expect(getByText(/en/i)).toBeInTheDocument();
 });
 
 test('Language change', () => {
-  const { getByText } = render(<Header />);
+  const { getByText } = render(<Header />, { wrapper: BrowserRouter });
   userEvent.click(getToggle());
   userEvent.click(getRuOption());
   userEvent.click(getToggle());
