48a118ebfe37372ad129a4821333170109b7c388
Muhammed Abdrahman <muhammed03amin.com>
2022-05-09 02:59:49 +0600

    fix: fix store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './layout/Layout/Layout';
@@ -7,31 +7,35 @@ import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import BoardsPage from './pages/BoardsPage/BoardsPage';
 import BoardDetailPage from './pages/BoardDetailPage/BoardDetailPage';
 import Page404 from './pages/Page404/Page404';
-import { setupStore } from './redux/store';
-import { Provider } from 'react-redux';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
-
-const store = setupStore();
+import SignUpView from './pages/SignUp/SignUpView';
+import { auth } from './redux/user/actions';
+import { useAppDispatch } from './redux/hooks';
+import LoginView from './pages/Login/LoginView';
 
 const App = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(auth);
+  });
   return (
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<WelcomePage />} />
-          <Route
-            element={
-              <ProtectedRoute token={localStorage.getItem('token') || ''} redirectedPath="/" />
-            }
-          >
-            <Route path="main" element={<MainPage />} />
-            <Route path="boards" element={<BoardsPage />} />
-            <Route path="board/:id" element={<BoardDetailPage />} />
-          </Route>
-          <Route path="*" element={<Page404 />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<WelcomePage />} />
+        <Route
+          element={
+            <ProtectedRoute token={localStorage.getItem('token') || ''} redirectedPath="/" />
+          }
+        >
+          <Route path="main" element={<MainPage />} />
+          <Route path="boards" element={<BoardsPage />} />
+          <Route path="board/:id" element={<BoardDetailPage />} />
+          <Route path="signup" element={<SignUpView />} />
+          <Route path="login" element={<LoginView />} />
         </Route>
-      </Routes>
-    </Provider>
+        <Route path="*" element={<Page404 />} />
+      </Route>
+    </Routes>
   );
 };
 
