323e553e548eac435361590ac9d52a89ccdc12c8
Maria Serykh <marie.serykh@gmail.com>
2022-05-19 09:08:33 +0200

    refactor: delete unused code, refactor repetitve code

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './n18i';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
