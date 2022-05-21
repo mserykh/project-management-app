b72fa30ab8acf0388e70f0d83fa67c7ad9739389
Sergey Mangilev <ssmangilev@gmail.com>
2022-05-21 14:34:23 +0200

    fix: fix after rebase

--- src/components/ColumnCard/ColumnCard.tsx
+++ src/components/ColumnCard/ColumnCard.tsx
@@ -216,7 +216,7 @@ function ColumnCard({ id, title, order, boardId }: ColumnCardProps): JSX.Element
     handleUpdateColumnTitle();
   };
 
-  dragRef(dropRef(ref));
+  dragRef(dropRef(dropRef1(ref)));
 
   return (
     <>
@@ -271,7 +271,7 @@ function ColumnCard({ id, title, order, boardId }: ColumnCardProps): JSX.Element
             </form>
           )}
         </div>
-        <div className="flex justify-between mb-2" ref={dropRef1}>
+        <div className="flex justify-between mb-2">
           <div
             className="flex gap-2"
             onClick={() => {
