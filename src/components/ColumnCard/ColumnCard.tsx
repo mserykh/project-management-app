17b445d76edee167023d8f4b2e3feed1addcf2a2
Maria Serykh <marie.serykh@gmail.com>
2022-05-22 15:23:59 +0200

    fix: fix errors

--- src/components/ColumnCard/ColumnCard.tsx
+++ src/components/ColumnCard/ColumnCard.tsx
@@ -246,15 +246,14 @@ function ColumnCard({ id, title, order, boardId }: ColumnCardProps): JSX.Element
                 placeholder="Please enter the column title"
                 errorText={'The title should contain at least 1 character'}
                 hasError={!!errors?.columnTitle}
-                classNameLabel="hidden"
                 inputData={register('columnTitle', {
                   required: true,
                   minLength: 1,
                   value: title || '',
                 })}
-                containerClassName="w-full m-0 float-left  mb-[25px]"
+                containerClassName="w-full m-0 float-left"
                 inputClassName="border w-full text-base border-solid border-[#AFB0B9] rounded-[999px] pl-23 focus:outline-0 pl-[24px] py-[11px]"
-                labelClassName={`inline-block text-base text-[black] float-left mb-[12px] font-semibold`}
+                labelClassName="hidden"
               />
               <div className="buttons-wrapper">
                 <Button className="button--cancel" type="button" onClick={handleCancel}>
