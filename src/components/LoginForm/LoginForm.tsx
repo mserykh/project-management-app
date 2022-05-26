144440ef430f533c72dee734fe940a5a0a9ef4f0
Muhammed Abdrahman <muhammed03amin.com>
2022-05-26 14:52:18 +0600

    feat: add Loader for Login/Signup

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import FormElement from '../FormElements/FormElement';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { useAppDispatch } from '../../redux/hooks';
@@ -7,6 +7,7 @@ import { signIn } from '../../redux/user/actions';
 import { useNavigate } from 'react-router';
 import store from '../../redux/store';
 import Button from '../Button/Button';
+import MiniLoader from '../../components/Loader/MiniLoader';
 
 interface LoginFormProps {
   labelColor: string;
@@ -26,9 +27,13 @@ const LoginForm: React.FC<LoginFormProps> = ({ labelColor }) => {
   const navigate = useNavigate();
   const { dispatch: toastDispatch } = useContext(ToastContext);
 
+  const [loading, setLoading] = useState(false);
+
   const formSubmitHandler: SubmitHandler<FieldValues> = async (values) => {
+    setLoading(true);
     const userData = { login: values.username, password: values.password };
     await dispatch(signIn(userData));
+    setLoading(false);
     const isAuthenticated = store.getState().userReducer.isAuthenticated;
     if (isAuthenticated) {
       navigate('/');
@@ -68,6 +73,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ labelColor }) => {
       />
       <Button className="button--signin" type="submit">
         Sign In
+        {loading && <MiniLoader />}
       </Button>
     </form>
   );
