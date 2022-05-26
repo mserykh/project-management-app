66eaa20357750fc13d021457ffced892ec351535
Muhammed Abdrahman <muhammed03amin.com>
2022-05-26 15:30:47 +0600

    feat: add MiniLoader for UserEditForm

--- src/components/Loader/MiniLoader.tsx
+++ src/components/Loader/MiniLoader.tsx
@@ -2,10 +2,10 @@ import React, { FC } from 'react';
 
 const MiniLoader: FC = () => {
   return (
-    <div className="inline relative">
+    <div className="inline relative top-[4px]">
       <div
         style={{ borderTopColor: 'transparent' }}
-        className="w-6 h-6 border-2 border-[#1bd993] border-solid rounded-full animate-spin ml-4 inline-block absolute top-[2px]"
+        className="w-6 h-6 border-2 border-[#1bd993] border-solid rounded-full animate-spin ml-4 inline-block"
       ></div>
     </div>
   );
