e33be76c0c774bb8814d9b0790b23d1ee059ab25
Muhammed Abdrahman <muhammed03amin.com>
2022-05-26 14:52:01 +0600

    feat: add Loader for BoardDetailPage

--- src/pages/BoardDetailPage/BoardDetailPage.tsx
+++ src/pages/BoardDetailPage/BoardDetailPage.tsx
@@ -7,6 +7,7 @@ import AddColumnForm from '../../components/AddColumnForm/AddColumnForm';
 import Button from '../../components/Button/Button';
 import ColumnCard from '../../components/ColumnCard/ColumnCard';
 import Modal from '../../components/Modal/Modal';
+import Loader from '../../components/Loader/Loader';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { fetchBoard, getAllUsers } from '../../redux/reducers/board/ActionsBoard';
 import { ColumnInterface } from '../../types';
@@ -22,7 +23,7 @@ function BoardDetailPage(): JSX.Element {
   const id = urlParams.id as string;
 
   const dispatch = useAppDispatch();
-  const { boardData } = useAppSelector((state) => state.boardReducer);
+  const { boardData, loading } = useAppSelector((state) => state.boardReducer);
 
   const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
 
@@ -44,42 +45,46 @@ function BoardDetailPage(): JSX.Element {
 
   return (
     <DndProvider backend={HTML5Backend}>
-      <section className="pt-6 grid grid-rows-columns gap-x-2 gap-y-6 mx-auto h-columns">
-        <header className="section__header section__header--columns">
-          <div className="section__header-inner">
-            <h2 className="section__title">{boardData.title}</h2>
-            <p className="section__description">{boardData.description}</p>
-          </div>
-          <Button
-            className="button--back"
-            type="button"
-            onClick={() => {
-              navigate('/main');
-            }}
-            isDisabled={false}
-          >
-            Go to boards list
-          </Button>
-        </header>
-        <section className="columns-list">
-          <ul className="grid grid-flow-col gap-4">{columnsRender}</ul>
-          <Button
-            className="button button--column"
-            type="button"
-            onClick={() => {
-              setIsModalOpened(true);
-            }}
-            isDisabled={false}
-          >
-            Add a column
-          </Button>
-          {isModalOpened && (
-            <Modal onClose={handleOnClose}>
-              <AddColumnForm onClose={handleOnClose} id={id} />
-            </Modal>
-          )}
+      {loading ? (
+        <Loader text="Board is loading..." />
+      ) : (
+        <section className="pt-6 grid grid-rows-columns gap-x-2 gap-y-6 mx-auto h-columns">
+          <header className="section__header section__header--columns">
+            <div className="section__header-inner">
+              <h2 className="section__title">{boardData.title}</h2>
+              <p className="section__description">{boardData.description}</p>
+            </div>
+            <Button
+              className="button--back"
+              type="button"
+              onClick={() => {
+                navigate('/main');
+              }}
+              isDisabled={false}
+            >
+              Go to boards list
+            </Button>
+          </header>
+          <section className="columns-list">
+            <ul className="grid grid-flow-col gap-4">{columnsRender}</ul>
+            <Button
+              className="button button--column"
+              type="button"
+              onClick={() => {
+                setIsModalOpened(true);
+              }}
+              isDisabled={false}
+            >
+              Add a column
+            </Button>
+            {isModalOpened && (
+              <Modal onClose={handleOnClose}>
+                <AddColumnForm onClose={handleOnClose} id={id} />
+              </Modal>
+            )}
+          </section>
         </section>
-      </section>
+      )}
     </DndProvider>
   );
 }
