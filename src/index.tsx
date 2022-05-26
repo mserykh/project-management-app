e2ff720790ceb248047a3159eb42e29cf7c609ad
Muhammed Abdrahman <muhammed03amin.com>
2022-05-26 14:56:44 +0600

    feat: change the text of Loader

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
-    <Suspense fallback={<Loader text="Loading" />}>
+    <Suspense fallback={<Loader text="Loading ..." />}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
